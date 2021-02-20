a1b0f3f96ddf0274dd106921f38f22dd326a6482
ts3486 <taos3486@gmail.com>
2021-02-20 15:59:34 +0900

    git init

---
+++ frontend/src/Components/Posts/PostsPageComponent_style.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const PostsPageWrapper = styled.div`
+
+    .formContainer{
+        display: flex;
+        justify-content: center;
+        margin-bottom: 200px;
+    }
+
+    .MuiButtonBase-root{
+        margin-top: 10px;
+        margin-bottom: 200px;
+        padding: 25px;
+    }
+
+`;
